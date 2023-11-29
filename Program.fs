66b30bb44f352078067f74fefba36a9a99a7fb78
Komalpreet kaur <komalsidhu1103@gmail.com>
2023-11-29 09:22:10 -0500

    version1

--- Program.fs
+++ Program.fs
@@ -1,2 +1,22 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Define the discriminated union for genres
+type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+// Define the "Director" record type
+type Director = {
+    Name: string
+    Movies: int
+}
+// Define the "Movie" record type
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    IMDBRating: float
+}
