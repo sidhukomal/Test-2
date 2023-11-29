0e235200cb0637c81a804ccab8e4807d9e555e6e
Komalpreet kaur <komalsidhu1103@gmail.com>
2023-11-29 09:40:32 -0500

    version1

--- Program.fs
+++ Program.fs
@@ -20,3 +20,13 @@ type Movie = {
     Director: Director
     IMDBRating: float
 }
+// Create movie instances
+let movies = [
+    { Name = "CODA"; RunLength = 111; Genre = Drama; Director = { Name = "Sian Heder"; Movies = 9 }; IMDBRating = 8.1 }
+    { Name = "Belfast"; RunLength = 98; Genre = Comedy; Director = { Name = "Kenneth Branagh"; Movies = 23 }; IMDBRating = 7.3 }
+    { Name = "Dune"; RunLength = 155; Genre = Fantasy; Director = { Name = "Denis Villeneuve"; Movies = 24 }; IMDBRating = 8.1 }
+    { Name = "King Richard"; RunLength = 144; Genre = Sport; Director = { Name = "Reinaldo Marcus Green"; Movies = 15 }; IMDBRating = 7.5 }
+    { Name = "Licorice Pizza"; RunLength = 133; Genre = Comedy; Director = { Name = "Paul Thomas Anderson"; Movies = 49 }; IMDBRating = 7.4 }
+    { Name = "Nightmare Alley"; RunLength = 150; Genre = Thriller ; Director = { Name = "Guillermo Del Toro"; Movies = 22 }; IMDBRating = 7.1 }
+]
+
